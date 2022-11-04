7edb3296cde01282d3cbbd7d6b409df90e1c5bd9
Viktor Kuroljov <viktor@ku.family>
2022-11-04 03:22:37 +0200

    chore: setup .pest

--- sven/lib-parser/src/lib.rs
+++ sven/lib-parser/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use pest::Parser;
+
+#[macro_use]
+extern crate pest_derive;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"] // relative to src
+struct CommitParser;
+
+pub fn commit_parser(commit: &str) -> () {
+    let res = CommitParser::parse(Rule::Input, commit).unwrap();
+    println!("{:#?}", res);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        commit_parser("something?");
+        assert_eq!(1, 2);
     }
 }
