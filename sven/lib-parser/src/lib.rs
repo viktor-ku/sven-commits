fe1f6273af64c8e74a56ed65d6b385ced4b8f430
Viktor Kuroljov <viktor@ku.family>
2022-11-04 04:10:35 +0200

    feat: setup flexible AST parsing for type

--- sven/lib-parser/src/lib.rs
+++ sven/lib-parser/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::{bail, Result};
 use pest::Parser;
 
 #[macro_use]
@@ -7,19 +8,26 @@ extern crate pest_derive;
 #[grammar = "grammar.pest"] // relative to src
 struct CommitParser;
 
-pub fn commit_parser(commit: &str) -> () {
-    let res = CommitParser::parse(Rule::Input, commit).unwrap();
-    println!("{:#?}", res);
+pub fn commit_parser(commit: &str) -> Result<()> {
+    match CommitParser::parse(Rule::Commit, commit) {
+        Ok(parsed) => {
+            println!("{:#?}", parsed);
+            Ok(())
+        }
+        Err(e) => {
+            bail!("{:#?}", e)
+        }
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod commits {
     use super::*;
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn it_works() {
-        commit_parser("something?");
+    fn must_start_with_a_type() {
+        commit_parser(": prevent wrong commits").unwrap();
         assert_eq!(1, 2);
     }
 }
