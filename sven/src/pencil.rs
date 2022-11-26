a229c80798aede1a6daae6b2e92888c440986dba
Viktor Kuroljov <viktor@ku.family>
2022-11-26 09:44:21 +0200

    refactor(pencil): simplify Ord impl

--- sven/src/pencil.rs
+++ sven/src/pencil.rs
@@ -1,7 +1,7 @@
 use crate::subject::Subject;
 use std::{cmp::Ordering, fmt::Debug};
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd)]
 pub struct Pencil {
     pub subject: Subject,
     pub found_at: Option<usize>,
@@ -9,28 +9,11 @@ pub struct Pencil {
     pub prev: Option<Subject>,
 }
 
-impl PartialOrd for Pencil {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self.found_at, other.found_at) {
-            (Some(me_at), Some(other_at)) => {
-                if me_at > other_at {
-                    Some(Ordering::Greater)
-                } else if me_at == other_at {
-                    Some(Ordering::Equal)
-                } else {
-                    Some(Ordering::Less)
-                }
-            }
-            _ => None,
-        }
-    }
-}
-
 impl Ord for Pencil {
     fn cmp(&self, other: &Self) -> Ordering {
-        match self.partial_cmp(other) {
-            Some(ord) => ord,
-            None => self.subject.cmp(&other.subject),
+        match (self.found_at, other.found_at) {
+            (Some(me_at), Some(other_at)) => me_at.cmp(&other_at),
+            _ => self.subject.cmp(&other.subject),
         }
     }
 }
