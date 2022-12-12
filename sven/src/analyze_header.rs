6c7009e284ca225f653858028d82ad84b869cd65
Viktor Kuroljov <viktor@ku.family>
2022-12-12 03:13:50 +0200

    test(analyze_header): define idea of type matching

--- sven/src/analyze_header.rs
+++ sven/src/analyze_header.rs
@@ -34,6 +34,8 @@ fn is_type(expected_type: &KnownType, actual_block: &Block, commit: &str) -> boo
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use super::*;
     use crate::{block_factory::BlockFactory, weak_commit::WeakCommit};
     use pretty_assertions::assert_eq;
@@ -45,15 +47,66 @@ mod tests {
         analyze_header(commit, config, w.header)
     }
 
-    /// "one" does not match any expected type value
     #[test]
-    fn a01() {
+    fn just_colon_is_missing_when_type_is_first_seq() {
         let blocks = with_commit(
             Config {
                 known_type: KnownType::AnyFirstSeq,
             },
             "one two three",
         );
-        assert_eq!(1, 2);
+
+        let f = {
+            let mut f = BlockFactory::new();
+            f.kind(1_000, "one")
+                .colon_missing(1_500)
+                .space(2_000)
+                .desc(3_000, "two three");
+            f
+        };
+
+        assert_eq!(f.blocks, blocks);
+    }
+
+    #[test]
+    fn just_colon_is_missing_when_type_is_known() {
+        let blocks = with_commit(
+            Config {
+                known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
+            },
+            "fix two three",
+        );
+
+        let f = {
+            let mut f = BlockFactory::new();
+            f.kind(1_000, "fix")
+                .colon_missing(1_500)
+                .space(2_000)
+                .desc(3_000, "two three");
+            f
+        };
+
+        assert_eq!(f.blocks, blocks);
+    }
+
+    #[test]
+    fn only_desc_found_when_could_not_find_type() {
+        let blocks = with_commit(
+            Config {
+                known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
+            },
+            "one two three",
+        );
+
+        let f = {
+            let mut f = BlockFactory::new();
+            f.kind_missing(250)
+                .colon_missing(500)
+                .space_missing(750)
+                .desc(1_000, "one two three");
+            f
+        };
+
+        assert_eq!(f.blocks, blocks);
     }
 }
