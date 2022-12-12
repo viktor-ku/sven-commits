6f4f57817dc6fbafb6c3c5385f5a8bffcffd515b
Viktor Kuroljov <viktor@ku.family>
2022-12-12 08:03:23 +0200

    test: try all tests

--- sven/src/analyze_header.rs
+++ sven/src/analyze_header.rs
@@ -202,45 +202,45 @@ mod tests {
         assert_eq!(f.blocks, blocks);
     }
 
-    // #[test]
-    // fn just_colon_is_missing_when_type_is_known() {
-    //     let blocks = with_commit(
-    //         &Config {
-    //             known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
-    //         },
-    //         "fix two three",
-    //     );
-    //
-    //     let f = {
-    //         let mut f = BlockFactory::new();
-    //         f.kind(1_000, "fix")
-    //             .colon_missing(1_500)
-    //             .space(2_000)
-    //             .desc(3_000, "two three");
-    //         f
-    //     };
-    //
-    //     assert_eq!(f.blocks, blocks);
-    // }
-    //
-    // #[test]
-    // fn only_desc_found_when_could_not_find_type() {
-    //     let blocks = with_commit(
-    //         &Config {
-    //             known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
-    //         },
-    //         "one two three",
-    //     );
-    //
-    //     let f = {
-    //         let mut f = BlockFactory::new();
-    //         f.kind_missing(250)
-    //             .colon_missing(500)
-    //             .space_missing(750)
-    //             .desc(1_000, "one two three");
-    //         f
-    //     };
-    //
-    //     assert_eq!(f.blocks, blocks);
-    // }
+    #[test]
+    fn just_colon_is_missing_when_type_is_known() {
+        let blocks = with_commit(
+            &Config {
+                known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
+            },
+            "fix two three",
+        );
+
+        let f = {
+            let mut f = BlockFactory::new();
+            f.kind(1_000, "fix")
+                .colon_missing(1_500)
+                .space(2_000)
+                .desc(3_000, "two three");
+            f
+        };
+
+        assert_eq!(f.blocks, blocks);
+    }
+
+    #[test]
+    fn only_desc_found_when_could_not_find_type() {
+        let blocks = with_commit(
+            &Config {
+                known_type: KnownType::Set(HashSet::from_iter(["fix".to_string()])),
+            },
+            "one two three",
+        );
+
+        let f = {
+            let mut f = BlockFactory::new();
+            f.kind_missing(250)
+                .colon_missing(500)
+                .space_missing(750)
+                .desc(1_000, "one two three");
+            f
+        };
+
+        assert_eq!(f.blocks, blocks);
+    }
 }
