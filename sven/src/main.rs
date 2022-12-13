2a3f98aa16bc6285e3644ef3b09bb6b4775fee8d
Viktor Kuroljov <viktor@ku.family>
2022-12-13 09:16:55 +0200

    refactor(at): delete at

--- sven/src/main.rs
+++ sven/src/main.rs
@@ -4,17 +4,16 @@ extern crate pest_derive;
 mod additive;
 mod analyze;
 mod analyze_header;
-mod at;
 mod block;
 mod block_factory;
 mod bytes;
+mod config;
 mod conventional_commit;
 mod domain;
 mod footer_issue;
 mod paper;
 mod pencil;
 mod report;
-mod config;
 mod weak_commit;
 
 use analyze::analyze;
